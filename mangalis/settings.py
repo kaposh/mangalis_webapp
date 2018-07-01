aa3b3b20a229d1e336e20799d8ec6af1f546fbe9
Marek <marek.bohdan1@gmail.com>
2018-07-01 21:40:30 +1200

    Update

--- mangalis/settings.py
+++ mangalis/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mangalis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mangalis', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
